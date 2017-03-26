a0bf2a17bd99b953b3547ad7609cf0b619fde236
vivek kumar <vivek kumar>
2017-03-26 10:24:16 +0530

    first commit

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include<iostream>  
+using namespace std;
+int circle(int );
+int main()
+{
+	int r,b;
+	cin>>r;
+	b=circle(r);
+}
+int  circle(int r)
+{
+	int a,p;
+	a=r*r*3.14;
+	p=r*4.28
+	cout<<a<<endl<<p;
+}
